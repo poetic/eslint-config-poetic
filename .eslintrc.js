1ca69fba0b2eb04594fdd7a6537a0b7b032d5813
Chun Yang <yang2007chun@gmail.com>
2016-07-08 15:16:33 -0500

    remove globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,8 @@ module.exports = {
   // Following AirBnB's lead
   "extends": "airbnb",
 
-  parserOptions: {
-    ecmaVersion: 6,
+  "parserOptions": {
+    "ecmaVersion": 6,
   },
 
   // And overriding where necessary
@@ -23,73 +23,8 @@ module.exports = {
     // particularly callbacks associated with Meteor.subscribe.
     "consistent-return": 0,
 
-    // We prefer to allow alerts/confirms because they function as desired 
+    // We prefer to allow alerts/confirms because they function as desired
     // across platforms (especially mobile)
     "no-alert": 0,
   },
-
-  // To configure global variables inside of a configuration file, use the
-  // globals key and indicate the global variables you want to use. Set each
-  // global variable name equal to true to allow the variable to be overwritten
-  // or false to disallow overwriting.
-  globals: {
-    // node globals
-    "util": false,
-
-    // Meteor core
-    "Meteor": false,
-    "DDP": false,
-    "Mongo": false, //Meteor.Collection renamed to Mongo.Collection
-    "Session": false,
-    "Accounts": false,
-    "Template": false,
-    "Blaze": false,  //UI is being renamed Blaze
-    "UI": false,
-    "Match": false,
-    "check": false,
-    "Tracker": false, //Deps renamed to Tracker
-    "Deps": false,
-    "ReactiveVar": false,
-    "EJSON": false,
-    "HTTP": false,
-    "Email": false,
-    "Assets": false,
-    "Handlebars": false,
-    "Package": false,
-    "App": false, //mobile-config.js
-    "Future": false,
-
-    // Meteor internals
-    "DDPServer": false,
-    "global": false,
-    "Log": false,
-    "MongoInternals": false,
-    "process": false,
-    "WebApp": false,
-    "WebAppInternals": false,
-
-    // globals useful when creating Meteor packages
-    "Npm": false,
-    "Tinytest": false,
-
-    // common Meteor packages
-    "Random": false,
-    "lodash": false,
-    "_": true,                // Set to true to allow redefining to Lodash.
-    "$": false,               // jQuery
-    "Router": false,          // iron-router
-    "FlowRouter": false,      // flow-router
-    "React": false,           // React
-    "ReactLayout": false,     // ReactLayout
-    "ReactRouter": false,
-    "ReactDOM": false,
-    "ReactMeteorData": false,
-
-    // App packages
-    "SimpleSchema": false,
-    "Roles": false,
-
-    // Collections
-    // PUT YOUR APP'S COLLECTION GLOBALS HERE
-  }
 };
